8ba364e0b69ba5b1dd00f5a73a660e1367b7651d
NZABANTERURA Jean Damascene <nzabajd135@gmail.com>
2026-01-21 22:24:40 +0200

    Add Grant Escrow smart contract skeleton
    Initial implementation of Grant Escrow smart contract with methods for creating grants, approving milestones, and releasing funds.

---
+++ contracts/grant_escrow.rs
@@ -0,0 +1,18 @@
+/ Grant Escrow Smart Contract
+// Locks funds and releases them after milestone approval
+
+pub struct GrantEscrow;
+
+impl GrantEscrow {
+    pub fn create_grant() {
+        // TODO: lock funds
+    }
+
+    pub fn approve_milestone() {
+        // TODO: approve milestone
+    }
+
+    pub fn release_funds() {
+        // TODO: release funds
+    }
+}
